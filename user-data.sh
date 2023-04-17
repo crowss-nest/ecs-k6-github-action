1ac9c5331be672cfcbad89b592f398a79e6b0fda
crowss-nest <wise.arcn@gmail.com>
2023-04-18 01:23:07 +0900

    new script

--- user-data.sh
+++ user-data.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-
-# Update package lists and install dependencies
-echo "Updating package lists and installing dependencies..."
-export DEBIAN_FRONTEND=noninteractive
-
 sudo apt update
 sudo apt install snapd
 
